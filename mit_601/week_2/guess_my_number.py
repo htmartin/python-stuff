aabdc5d0868462ab6b9ab45a6ba8fe6f9324afed
Taylor Martin <tmartin@oreilly.com>
2018-01-23 09:12:30 -0700

    move mit 601

--- mit_601/week_2/guess_my_number.py
+++
@@ -1,3 +0,0 @@
-print('Please think of a number between 0 and 100!')
-print('Is your secret number 50?')
-print('Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.')
